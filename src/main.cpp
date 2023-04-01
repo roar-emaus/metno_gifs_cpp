053bc043a59f700cb07bc8f1dc12beb653612ad8
Roar Emaus <roar@emaus.no>
2023-04-01 16:33:33 +0200

    split up functionality into multiple functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
 #include "create_images.h"
 #include "ThreadPool.h"
 
-
-int main(int argc, char *argv[]) {
-    std::string input_file;
-    std::string variable_or_threads;
-    std::string output_folder = "output";
-
+bool parse_arguments(int argc, char *argv[], std::string& input_file, std::string& variable_or_threads, std::string& output_folder) {
     for (int i = 1; i < argc; i += 2) {
         if (i + 1 >= argc) {
             std::cerr << "Error: Missing value for argument " << argv[i] << std::endl;
-            return 1;
+            return false;
         }
         if (strcmp(argv[i], "--input") == 0) {
             input_file = argv[i + 1];
@@ -20,17 +15,49 @@ int main(int argc, char *argv[]) {
             output_folder = argv[i + 1];
         } else {
             std::cerr << "Error: Unknown argument " << argv[i] << std::endl;
-            return 1;
+            return false;
         }
     }
 
     if (input_file.empty()) {
         std::cerr << "Error: Missing required argument --input" << std::endl;
-        return 1;
+        return false;
     }
 
     if (variable_or_threads.empty()) {
         std::cerr << "Error: Missing required argument --var_or_threads" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void process_single_variable(const std::string& input_file, const std::string& variable, const std::string& alias, const std::string& output_folder) {
+    std::filesystem::create_directories(output_folder);
+    visualize_variable(input_file, variable, alias, output_folder);
+    const std::string& input_pattern = output_folder + "/" + "*.jpg";
+    const std::string& output_filename = output_folder + "/" + alias + ".gif";
+    create_gif(input_pattern, output_filename, 10);
+}
+
+void process_variables_threaded(int num_threads, const std::map<std::string, std::string>& variable_aliases, const std::string& input_file, const std::string& output_folder) {
+    ThreadPool thread_pool(num_threads);
+    for (const auto& alias_var_pair : variable_aliases) {
+        const std::string& alias = alias_var_pair.first;
+        const std::string& variable = alias_var_pair.second;
+        const std::string& variable_output_folder = output_folder + "/" + alias;
+        thread_pool.enqueue([input_file, variable, alias, variable_output_folder]() {
+            process_single_variable(input_file, variable, alias, variable_output_folder);
+        });
+    }
+}
+
+int main(int argc, char *argv[]) {
+    std::string input_file;
+    std::string variable_or_threads;
+    std::string output_folder = "output";
+
+    if (!parse_arguments(argc, argv, input_file, variable_or_threads, output_folder)) {
         return 1;
     }
 
@@ -48,17 +75,7 @@ int main(int argc, char *argv[]) {
     try {
         int num_threads = std::stoi(variable_or_threads);
         if (num_threads > 0) {
-            ThreadPool thread_pool(num_threads);
-            for (const auto& alias_var_pair : variable_aliases) {
-                const std::string& alias = alias_var_pair.first;
-                const std::string& variable = alias_var_pair.second;
-                const std::string& variable_output_folder = output_folder + "/" + alias;
-                std::filesystem::create_directories(variable_output_folder);
-                thread_pool.enqueue(visualize_variable, input_file, variable, alias, variable_output_folder);
-                const std::string& input_pattern = variable_output_folder +"/"+ "*.jpg";
-                const std::string& output_filename = variable_output_folder +"/"+ alias + ".gif";
-                thread_pool.enqueue(create_gif, input_pattern, output_filename, 10);
-            }
+            process_variables_threaded(num_threads, variable_aliases, input_file, output_folder);
         } else {
             std::cerr << "Error: Invalid number of threads." << std::endl;
             return 1;
@@ -67,11 +84,7 @@ int main(int argc, char *argv[]) {
         auto it = variable_aliases.find(variable_or_threads);
         if (it != variable_aliases.end()) {
             const std::string& variable_output_folder = output_folder + "/" + it->first;
-            std::filesystem::create_directories(variable_output_folder);
-            visualize_variable(input_file, it->second, it->first, output_folder);
-            const std::string& input_pattern = variable_output_folder +"/"+ "*.jpg";
-            const std::string& output_filename = variable_output_folder + "/" + it->first + ".gif";
-            create_gif(input_pattern, output_filename, 10);
+            process_single_variable(input_file, it->second, it->first, variable_output_folder);
         } else {
             std::cerr << "Error: Invalid variable name or number of threads provided." << std::endl;
             return 1;
@@ -79,4 +92,5 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
-} 
+}
+
